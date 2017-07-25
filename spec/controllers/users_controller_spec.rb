9cbcdd77ee86b33c703282ae24c04776df5efff7
synion <lach.szymon86@gmail.com>
2017-07-25 11:10:38 +0200

    Test login before see user listing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,12 +3,16 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   let(:user) { create :user }
 
-  before { sign_in user }
-
-  describe 'GET #index' do
+  describe 'GET #index with login' do
+    before { sign_in user }
     subject { get :index }
 
     it_behaves_like 'template rendering action', :index
   end
 
+  describe 'GET #index without login' do
+    subject { get :index }
+
+    it { is_expected.to redirect_to new_user_session_path }
+  end
 end
