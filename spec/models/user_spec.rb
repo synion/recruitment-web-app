f19af9a06e222497ea753ea2a5dab56330825fac
synion <lach.szymon86@gmail.com>
2017-07-25 12:10:14 +0200

    Test admin column on User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe User, type: :model do
   describe 'database column' do
     it { should have_db_column :email }
     it { should have_db_column :encrypted_password }
+    it { should have_db_column :admin }
   end
 end
