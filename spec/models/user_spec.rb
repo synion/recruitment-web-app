c8a9e7f3d03c2284a5d7c533f1a37d99593e3290
synion <lach.szymon86@gmail.com>
2017-07-25 09:52:49 +0200

    Test for model required for first task

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'database column' do
+    it { should have_db_column :email }
+    it { should have_db_column :encrypted_password }
+  end
+end
