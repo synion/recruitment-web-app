eb74615bc4481b1ccfbdb05169682166c9f07d6c
synion <lach.szymon86@gmail.com>
2017-07-25 19:51:58 +0200

    Add Delete for Admin and on index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-  def index
+  expose :users, ->{ User.all }
+  expose :user
+  before_action :authenticate_user!, only: :index
+
+   def destroy
+    authorize user
+    user.destroy
+    redirect_to users_path
+    flash[:danger] = "User was deleted"
   end
 end
