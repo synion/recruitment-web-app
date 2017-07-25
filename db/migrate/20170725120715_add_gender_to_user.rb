23b347f3eb466f406d5440344ecfb2e4caafa976
synion <lach.szymon86@gmail.com>
2017-07-25 22:30:23 +0200

    Change gender in DB

--- db/migrate/20170725120715_add_gender_to_user.rb
+++ db/migrate/20170725120715_add_gender_to_user.rb
@@ -1,17 +1,5 @@
 class AddGenderToUser < ActiveRecord::Migration[5.0]
-  def up
-    execute <<-SQL
-      CREATE TYPE gender AS ENUM ('male', 'female');
-    SQL
-
-    add_column :users, :gender, :gender, index: true
-  end
-
-  def down
-    remove_column :users, :gender
-
-    execute <<-SQL
-      DROP TYPE gender;
-    SQL
+  def change
+    add_column :users, :gender, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,24 @@ ActiveRecord::Schema.define(version: 20170725122139) do
     t.index ["user_id"], name: "index_interests_on_user_id", using: :btree
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'gender' for column 'gender'
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "admin",                  default: false
+    t.integer  "age"
+    t.string   "gender"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
 
 end
