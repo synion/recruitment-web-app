7c1955cee8c36d4936bce9d1d96decfd530eacbc
synion <lach.szymon86@gmail.com>
2017-07-25 14:40:55 +0200

    Add test model interest and user

---
+++ spec/models/interest_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Interest, type: :model do
+  describe 'database column' do
+    it { should have_db_column :user_id }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to :user }
+  end
+
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe User, type: :model do
     it { should have_db_column :email }
     it { should have_db_column :encrypted_password }
     it { should have_db_column :admin }
+    it { should have_db_column :gender }
+    it { should have_db_column :age }
   end
+
+  describe 'associations' do
+    it { is_expected.to have_many :interests }
+  end
+
 end
+
