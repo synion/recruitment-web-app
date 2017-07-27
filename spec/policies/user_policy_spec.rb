11214f5d92c1aa4734e8c515bf6c53c2bdd05c6c
synion <lach.szymon86@gmail.com>
2017-07-27 12:23:19 +0200

    Add polcies spec for chec autorization actions

---
+++ spec/policies/user_policy_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe UserPolicy do
+
+  let(:admin) { create :admin }
+  let(:user) { create :user }
+  let(:check_user) { create :user, email: "example@pls.pl", password: "password"}
+
+  context 'for regular user' do
+    subject { UserPolicy.new(user,check_user) }
+
+    it { should_not permit(:destroy) }
+
+    it { should permit(:index) }
+  end
+
+  context 'for admin user' do
+    subject { UserPolicy.new(admin,check_user) }
+    it { should permit(:destroy) }
+    it { should permit(:index) }
+  end
+end
