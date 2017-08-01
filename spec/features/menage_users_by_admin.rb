d9a624ad30b1230c3c61eedf95900ff3e4daa4ef
synion <lach.szymon86@gmail.com>
2017-08-01 18:07:51 +0200

    Change spec for admin menagment

--- spec/features/menage_users_by_admin.rb
+++ spec/features/menage_users_by_admin.rb
@@ -22,8 +22,8 @@ feature 'MenageUsersByAdmin'  do
     page.all('td > a')[0].click
     page.driver.browser.switch_to.alert.accept
     sleep 1
-    expect { page.to_not have_content("example22@o2.pl")
-             page.to have_content("User was deleted") }
+    expect(page).to_not have_content("example22@o2.pl")
+    expect(page).to have_content("User was deleted")
   end
 
   scenario 'admin cancel delete user'  do
@@ -41,40 +41,41 @@ feature 'MenageUsersByAdmin'  do
 
   scenario 'admin can add new user' do
     visit admin_users_path
-    expect{ page.to_not have_content("new_user@o2.pl")
-            page.to_not have_content('33')
-            page.to_not have_content("female")
-            page.to_not have_content("Programing")
-            page.to_not have_content("work") }
+    expect(page).to_not have_content("new_user@o2.pl")
+    expect(page).to_not have_content('33')
+    expect(page).to_not have_content("female")
+    expect(page).to_not have_content("Programing")
+    expect(page).to_not have_content("work")
     create_user
-    expect{ page.to have_content("new_user@o2.pl")
-            page.to have_content('33')
-            page.to have_content("female")
-            page.to have_content("Programing")
-            page.to have_content("work") }
+    visit admin_users_path
+    expect(page).to have_content("new_user@o2.pl")
+    expect(page).to have_content('33')
+    expect(page).to have_content("female")
+    expect(page).to have_content("Programing")
+    expect(page).to have_content("work")
   end
 
   scenario 'admin can edit new user' do
     create_user
-    expect{ page.to have_content("new_user@o2.pl")
-            page.to have_content('33')
-            page.to have_content("female")
-            page.to have_content("Programing")
-            page.to have_content("work") }
+    visit admin_users_path
+    expect(page).to have_content("new_user@o2.pl")
+    expect(page).to have_content('33')
+    expect(page).to have_content("female")
+    expect(page).to have_content("Programing")
+    expect(page).to have_content("work")
     edit_user
     visit admin_users_path
-    expect{ page.to_not have_content("new_user@o2.pl")
-            page.to_not have_content('33')
-            page.to_not have_content("female")
-            page.to_not have_content("Programing")
-            page.to_not have_content("work")
+    expect(page).to_not have_content("new_user@o2.pl")
+    expect(page).to_not have_content('33')
+    expect(page).to_not have_content("female")
+    expect(page).to_not have_content("Programing")
+    expect(page).to_not have_content("work")
 
-            page.to have_content("new13_user@o2.pl")
-            page.to have_content('13')
-            page.to have_content("male")
-            page.to have_content("Snowboarding")
-            page.to have_content("health")
-          }
+    expect(page).to have_content("new13_user@o2.pl")
+    expect(page).to have_content('13')
+    expect(page).to have_content("male")
+    expect(page).to have_content("Snowboarding")
+    expect(page).to have_content("health")
   end
 end
 
