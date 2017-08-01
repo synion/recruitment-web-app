f75ed0067bfea7a117e23ee3c205c67a5e67bbdc
synion <lach.szymon86@gmail.com>
2017-08-01 14:12:24 +0200

    Change routest for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  resources :users, only: [:index, :destroy]
   root 'users#index'
   namespace :admin do
     get '', to: 'dashboard#index', as: '/'
+    resources :users, except: [:show,:destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
