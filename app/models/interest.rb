82ddabf25e8c35d8460a1732ed7dda5a6b0705bc
synion <lach.szymon86@gmail.com>
2017-08-02 22:22:10 +0200

    Change self method to scope

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -2,11 +2,7 @@ class Interest < ApplicationRecord
   self.inheritance_column = :_type_disabled
   enum type: { health: 0, hobby: 1, work: 2 }
   belongs_to :user
-
-  private
-
-  def self.number_of_interest_by_health
-    self.where("type = 0 and name like 'cosm%'").
-    where(user_id: User.where('age between 20 and 30 AND gender = 1')).count
-  end
+  scope :number_of_interest_by_health, -> { where("type = 0 and name like 'cosm%'").
+                                            where(user_id: User.where('age between 20 and 30 AND gender = 1')).
+                                            count }
 end
