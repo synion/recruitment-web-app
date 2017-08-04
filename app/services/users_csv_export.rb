941fa7e12292a3d5f5567b4ac37534854feb1408
synion <lach.szymon86@gmail.com>
2017-08-04 20:14:38 +0200

    Creat User csv exporter

---
+++ app/services/users_csv_export.rb
@@ -0,0 +1,32 @@
+require 'csv'
+
+class UsersCsvExport
+  ATTRIBUTES = %w{email age gender}
+
+  def initialize(users)
+    @users = users
+  end
+
+  def generate
+    CSV.generate do |csv|
+      csv << ATTRIBUTES + ["Interests"]
+      @users.each do |user|
+        csv << user_column(user)
+      end
+    end
+  end
+
+  private
+
+  def user_column(user)
+    attribute_columns(user) + interest_column(user)
+  end
+
+  def attribute_columns(user)
+    ATTRIBUTES.map { |column| user.send(column) }
+  end
+
+  def interest_column(user)
+    [user.interests.pluck(:name).join(";")]
+  end
+end
