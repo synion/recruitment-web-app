17990b4e929e86e35f17f90b5b44b8aa66e65698
synion <lach.szymon86@gmail.com>
2017-08-08 22:15:43 +0200

    Add new spec for mailers and services send regards

---
+++ spec/services/send_regard_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe SendRegard do
+  context "#call" do
+    let(:user) { create :user, email: "example@o2.pl", password: "password" }
+    let(:current_user) { create :user, email: "example2@o2.pl", password: "password" }
+
+
+    it "should have required arguments" do
+      regard_mailer = double("RegardMailer")
+      expect(RegardMailer).to receive(:send_regard).with( current_user_email: current_user.email,
+                                         user_email: user.email ) { regard_mailer }
+      expect(regard_mailer).to receive(:deliver)
+
+      SendRegard.new(user_id: user.id, current_user_id: current_user.id).call
+    end
+  end
+end
